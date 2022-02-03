5ce0b0ce02f207d99647c0dfa768cee787226c1f
rijan_chapagain <hrijon.chp@gmail.com>
2022-02-03 21:39:40 +0800

    Adds custom table creator and export to pdf

--- ExportPdf/Program.cs
+++ ExportPdf/Program.cs
@@ -26,8 +26,47 @@ namespace ExportPdf
             // Choose font to be used on the text
             XFont font = new XFont("Arial", 20);
 
+            xGraphics.DrawLine(new XPen(XColor.FromArgb(50, 30, 200)), new XPoint(50, 290), new XPoint(550, 290));
+
+            // generate Header for table
+            xGraphics.DrawString("First Name", font, XBrushes.Black, new XPoint(100,280));
+            xGraphics.DrawString("First Name", font, XBrushes.Black, new XPoint(250,280));
+            xGraphics.DrawString("First Name", font, XBrushes.Black, new XPoint(400,280));
+            xGraphics.DrawLine(new XPen(XColor.FromArgb(50, 30, 200)), new XPoint(50, 290), new XPoint(550, 290));
+
+            int currentYposition_value = 303;
+            int currentYposition_line = 310;
+
+            /*count number of rows to display in one table
+             * TODO: Add statement to control new page
+              if(count <= 20)
+            {
+
+            }*/
+
+            for(int i = 0; i < 20; i++)
+            {
+                xGraphics.DrawString("FirstName", font, XBrushes.Black, new XPoint(100, currentYposition_value));
+                xGraphics.DrawString("FirstName", font, XBrushes.Black, new XPoint(250, currentYposition_value));
+                xGraphics.DrawString("FirstName", font, XBrushes.Black, new XPoint(400, currentYposition_value));
+                xGraphics.DrawLine(new XPen(XColor.FromArgb(50, 30, 200)), new XPoint(50, currentYposition_line), new XPoint(550, currentYposition_line));
+
+                currentYposition_value += 20;
+                currentYposition_line += 20;
+            }
+
+            /*
+             * TODO: Add statement to control new page
+             else {
+
+                        same + 
+            count.Remove(count[i])
+
+                        page = pdfDocument.AddPage();
+            xGraphics = XGraphics.FromPdfPage(page);
+            }
+             */
             xGraphics.DrawString("First line of Text", font, XBrushes.Black, new XRect(0,0, page.Width, page.Height), XStringFormats.TopLeft);
-            xGraphics.DrawString("Second line of Text", font, XBrushes.Black, new XRect(0,0, page.Width, page.Height), XStringFormats.Center);
 
             pdfDocument.Save("test.pdf");
             Console.WriteLine("Document saved successfullly");
@@ -35,3 +74,17 @@ namespace ExportPdf
         }
     }
 }
+
+/*
+// The class will provide drawing related methods for specified page
+XGraphics xGraphics = XGraphics.FromPdfPage(page);
+
+// Choose font to be used on the text
+XFont font = new XFont("Arial", 20);
+
+xGraphics.DrawString("First line of Text", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.TopLeft);
+xGraphics.DrawString("Second line of Text", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+
+pdfDocument.Save("test.pdf");
+Console.WriteLine("Document saved successfullly");
+Console.ReadLine();*/
